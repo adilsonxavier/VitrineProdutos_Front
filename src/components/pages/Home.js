3d3bba8c1fadc8b065784fb9ae30e88c4023ba49
adilsonxavier <adilsonxdesouza@gmail.com>
2022-03-09 23:05:56 -0300

    fix_hone

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,10 +6,10 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <section className={styles.home_container}>
-
             <h1>Bem vindo ao site Vitrine de Produtos</h1>
-            <p style={{ marginTop: "1.5em", marginBottom: "0" }}>Login da área adminstrativa:</p>
-            <p style={{ marginTop: "1.5em", marginBottom: "0" }}><strong>usuário: </strong> maysa</p>
+            <h2 style={{fontSize:"1rem"}}>Projeto pessoal em React.js com back-end utilizando API em C# (.Net 5) com Entity Framework Core e Authenticação JWT</h2>
+            <p style={{ marginTop: "1rem", marginBottom: "0" }}>Login da área adminstrativa:</p>
+            <p style={{ marginTop: "1rem", marginBottom: "0" }}><strong>usuário: </strong> maysa</p>
             <p style={{ marginTop: "1em", marginBottom: "0" }}><strong>senha: </strong>henrique123</p>
 
             
